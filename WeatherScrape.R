b4449f72acd142f162304a92db1919b8d4a4f1bf
Adam Brooker <adambrooker1@hotmail.com>
2018-10-22 17:01:10 +0100

    New code for converting compass directions to degrees

--- WeatherScrape.R
+++ WeatherScrape.R
@@ -11,7 +11,7 @@ library(stringr)
 library(tidyverse)
 
 
-setwd('G:/Data/WeatherData/Data tables/Corran') 
+setwd('G:/Data/WeatherData/Data tables/Rahoy') 
 
 
 
@@ -32,7 +32,11 @@ startlink <- 'https://www.wunderground.com/personal-weather-station/dashboard?ID
 # Corran start link
 startlink <- 'https://www.wunderground.com/personal-weather-station/dashboard?ID=IUNITEDK518&cm_ven=localwx_pwsdash#history/s20160601/e20160601/mdaily'
 
-nodays <- 45
+# Rahoy start link
+startlink <- 'https://www.wunderground.com/personal-weather-station/dashboard?ID=IHIGHLAN16&cm_ven=localwx_pwsdash#history/tdata/s20180528/e20180528/mdaily'
+
+
+nodays <- 32
 
 remDr$navigate(startlink) # nagivate to start url
 #Sys.sleep(signif(runif(1,2,3), 2))
@@ -89,9 +93,10 @@ wdata <- rbind(wdata, day)
 remDr$findElement(using="css selector", value="#next-timeframe")$clickElement()
 Sys.sleep(signif(runif(1,5,7), 2))
 
-}
+} # end of weather scraping loop
 
-colnames(wdata) <- c('time', 'temp_C', 'dewpoint_C', 'humidity_percent', 'windDir_deg', 'windSpeed_kph', 'pressure_hPa', 
+
+colnames(wdata) <- c('time', 'temp_C', 'dewpoint_C', 'humidity_percent', 'windDir_deg', 'windSpeed_kph', 'windGust_kph', 
                      'pressure_hPa', 'precipRate_mm', 'precipAccum_mm', 'uvIndex', 'solarIrradiance_wperm2')
 
 wdata <- as.tibble(wdata)
@@ -101,20 +106,20 @@ wdata$temp_C <- as.numeric(str_sub(wdata$temp_C, 1, str_length(wdata$temp_C)-3))
 wdata$dewpoint_C <- as.numeric(str_sub(wdata$dewpoint_C, 1, str_length(wdata$dewpoint_C)-3))
 wdata$humidity_percent <- as.numeric(str_sub(wdata$humidity_percent, 1, str_length(wdata$humidity_percent)-2))
 wdata$windDir_deg <- ifelse(as.character(wdata$windDir_deg == 'North'), 0,
-                            ifelse(as.character(wdata$windDir_deg == 'NNE'), 22.5, 
-                                   ifelse(as.character(wdata$windDir_deg == 'NE'), 45, 
-                                          ifelse(as.character(wdata$windDir_deg == 'ENE'), 67.5, 
-                                                 ifelse(as.character(wdata$windDir_deg == 'East'), 90,
-                                                        ifelse(as.character(wdata$windDir_deg == 'ESE'), 112.5,
-                                                               ifelse(as.character(wdata$windDir_deg == 'SE'), 135,
-                                                                      ifelse(as.character(wdata$windDir_deg == 'SSE'), 157.5,
-                                                                             ifelse(as.character(wdata$windDir_deg == 'South'), 180,
-                                                                                    ifelse(as.character(wdata$windDir_deg == 'SSW'), 202.5,
-                                                                                           ifelse(as.character(wdata$windDir_deg == 'SW'), 225,
-                                                                                                  ifelse(as.character(wdata$windDir_deg == 'WSW'), 247.5,
-                                                                                                         ifelse(as.character(wdata$windDir_deg == 'West'), 270,
-                                                                                                                ifelse(as.character(wdata$windDir_deg == 'WNW'), 292.5,
-                                                                                                                       ifelse(as.character(wdata$windDir_deg == 'NW'), 315, 337.5)))))))))))))))
+                        ifelse(as.character(wdata$windDir_deg == 'NNE'), 22.5, 
+                          ifelse(as.character(wdata$windDir_deg == 'NE'), 45, 
+                              ifelse(as.character(wdata$windDir_deg == 'ENE'), 67.5, 
+                                  ifelse(as.character(wdata$windDir_deg == 'East'), 90,
+                                      ifelse(as.character(wdata$windDir_deg == 'ESE'), 112.5,
+                                          ifelse(as.character(wdata$windDir_deg == 'SE'), 135,
+                                              ifelse(as.character(wdata$windDir_deg == 'SSE'), 157.5,
+                                                  ifelse(as.character(wdata$windDir_deg == 'South'), 180,
+                                                     ifelse(as.character(wdata$windDir_deg == 'SSW'), 202.5,
+                                                        ifelse(as.character(wdata$windDir_deg == 'SW'), 225,
+                                                           ifelse(as.character(wdata$windDir_deg == 'WSW'), 247.5,
+                                                               ifelse(as.character(wdata$windDir_deg == 'West'), 270,
+                                                                  ifelse(as.character(wdata$windDir_deg == 'WNW'), 292.5,
+                                                                     ifelse(as.character(wdata$windDir_deg == 'NW'), 315, 337.5)))))))))))))))
 wdata$windSpeed_kph <- as.numeric(str_sub(wdata$windSpeed_kph, 1, str_length(wdata$windSpeed_kph)-4))
 wdata$windGust_kph <- as.numeric(str_sub(wdata$windGust_kph, 1, str_length(wdata$windGust_kph)-4))
 wdata$pressure_hPa <- as.numeric(str_sub(wdata$pressure_hPa, 1, str_length(wdata$pressure_hPa)-4))
@@ -123,7 +128,7 @@ wdata$precipAccum_mm <- as.numeric(str_sub(wdata$precipAccum_mm, 1, str_length(w
 wdata$uvIndex <- as.numeric(wdata$uvIndex)
 wdata$solarIrradiance_wperm2 <- as.numeric(str_sub(wdata$solarIrradiance_wperm2, 1, str_length(wdata$solarIrradiance_wperm2)-5))
 
-write.csv(wdata, 'CorranWeather_Jun-Nov2016.csv')
+write.csv(wdata, 'Rahoy_56_636N_5_841W_Weather_Jun-Aug2018.csv')
 
 rm(day, dew, hum, obtime, precipAccum, precipRate, pressure, solar, temp, uv, windDir, windGust, windSpeed, date, elems)
 
